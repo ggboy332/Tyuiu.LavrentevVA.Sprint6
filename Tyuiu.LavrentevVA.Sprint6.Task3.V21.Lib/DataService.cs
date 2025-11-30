86fc48302d63d4298c2968024a657896eb21d3ab
Vlad <Vlad@VLAD_X>
2025-11-30 15:49:53 +0500

    Task3

---
+++ Tyuiu.LavrentevVA.Sprint6.Task3.V21.Lib/DataService.cs
@@ -0,0 +1,31 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+namespace Tyuiu.LavrentevVA.Sprint6.Task3.V21.Lib
+{
+    public class DataService : ISprint6Task3V21
+    {
+        public int[,] Calculate(int[,] matrix)
+        {
+            //Дан массив 5 на 5 элементов. Выполнить сортировку по возрастанию в первом столбце. Результат вывести в DataGridView. Графический интерфейс оформить по шаблону из лекции.  26 -15   7   1  -4   14  26  22  17  27   -8  31  22  34   4  -18  16  -2  16  27    5   2  -4  16  15
+            
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
+            for (int i = 0; i < rows - 1; i++)
+            {
+                for (int j = 0; j < rows - i - 1; j++)
+                {
+                    if (matrix[j, 0] > matrix[j + 1, 0])
+                    {
+                        // Swap entire rows
+                        for (int k = 0; k < cols; k++)
+                        {
+                            int temp = matrix[j, k];
+                            matrix[j, k] = matrix[j + 1, k];
+                            matrix[j + 1, k] = temp;
+                        }
+                    }
+                }
+            }
+            return matrix;
+        }
+    }
+}
